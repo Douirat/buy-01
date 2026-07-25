1341da07ea6fc4d7e171f067d0eec8b1b73b4969
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-07-25 18:47:37 +0100

    jwt generator

---
+++ backend/user-service/src/main/java/com/zone01/buy01/user_service/jwt/PasswordEncoderConfig.java
@@ -0,0 +1,15 @@
+package com.zone01.buy01.user_service.jwt;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordEncoderConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
