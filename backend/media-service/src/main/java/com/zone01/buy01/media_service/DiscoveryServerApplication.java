924d0a4391779fbb1e58fbf4d3e776a59df391b7
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-08-02 13:27:10 +0100

    media service was registereed successfully to the eureka

--- backend/media-service/src/main/java/com/zone01/buy01/media_service/DiscoveryServerApplication.java
+++ backend/media-service/src/main/java/com/zone01/buy01/media_service/DiscoveryServerApplication.java
@@ -1,15 +1,14 @@
-package com.zone01.buy01.discovery_server;
+package com.zone01.buy01.media_service;
 
-// https://www.geeksforgeeks.org/java/service-discovery-and-service-registry-in-microservices/
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+// https://www.geeksforgeeks.org/java/service-discovery-and-service-registry-in-microservices/
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServerApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
 }
---
+++ backend/media-service/src/main/java/com/zone01/buy01/media_service/EnableEurekaServer.java
@@ -0,0 +1,5 @@
+package com.zone01.buy01.media_service;
+
+public @interface EnableEurekaServer {
+
+}
